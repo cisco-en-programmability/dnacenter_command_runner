b013bffb42a3d8e8aafbda03a87b562f507c4f61
gzapodea <gzapodea@cisco.com>
2019-09-03 18:36:34 -0700

    initial commit

---
+++ config.py
@@ -0,0 +1,35 @@
+"""
+
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+
+__author__ = "Gabriel Zapodeanu TME, ENB"
+__email__ = "gzapodea@cisco.com"
+__version__ = "0.1.0"
+__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+
+# This file contains the:
+# Cisco DNA Center username and password, server info
+
+# Update this section with the DNA Center server info and user information
+
+DNAC_URL = 'https://Cisco DNA Center Server info'
+DNAC_USER = 'username'
+DNAC_PASS = 'password'
+
